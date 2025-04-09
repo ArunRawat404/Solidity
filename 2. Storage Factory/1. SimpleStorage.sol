3adc8572a5367a8ce759fbef5c5cd3242eecdf41
Arun Rawat <arunrawatar404@gmail.com>
2025-04-09 18:07:08 +0530

    feat: add StorageFactory to deploy and manage SimpleStorage contracts

--- 2. Storage Factory/1. SimpleStorage.sol
+++ 2. Storage Factory/1. SimpleStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18; // solidity compiler version
+pragma solidity ^0.8.18; // solidity compiler version
 
 contract SimpleStorage {
     // uint is initialized to 0 if no value is given
--- 2. Storage Factory/2. StorageFactory.sol
+++ 2. Storage Factory/2. StorageFactory.sol
@@ -5,11 +5,21 @@ pragma solidity ^0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
         // The new keyword is used to deploy a new contract from within another contract.
-        //  It returns the address of the newly deployed contract. 
-        simpleStorage = new SimpleStorage();
+        //  It returns the address of the newly deployed contract.
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    // In order to interact with contract we need Address and ABI(Application Binary Interface)
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
