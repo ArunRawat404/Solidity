62df2ebcffc4664ab9e65a379e8bcece26f3c741
Arun Rawat <arunrawatar404@gmail.com>
2025-04-10 23:36:36 +0530

    feat: create PriceConverter library and refactor FundMe to use it

--- 3. Fund Me/1. FundMe.sol
+++ 3. Fund Me/1. FundMe.sol
@@ -2,9 +2,12 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./2. PriceConverter.sol";
 
 contract FundMe{
+    // Attach all the functions from the PriceConverter library to the uint256 type.
+    using PriceConverter for uint256;
+
     uint256 minimumUsd = 5e18;
 
     address[] public funders;
@@ -15,7 +18,9 @@ contract FundMe{
         // require(condition, errorMessage). require() reverts the whole transaction, including all state changes from prior function calls
         // msg.value is the amount of Ether (in wei) sent to the function.
         // 1e18 = 1 eth = 1000000000000000000 wei = 1 *10 ** 18 wei
-        require(getConversionRate(msg.value) >= minimumUsd, "Send minimum 5$ worth of eth");
+        // Under the hood msg.value.getConversionRate() converts to PriceConverter.getConversionRate(msg.value)
+        // In case of multiple arguments, first argument is always the value you're calling from (in this case msg.value), and the rest you pass normally.
+        require(msg.value.getConversionRate() >= minimumUsd, "Send minimum 5$ worth of eth");
         // msg.sender refers to the address that called the function
         funders.push(msg.sender);
         // When accessing a mapping for a new key, Solidity returns the default value (0 for uint256)
@@ -23,20 +28,4 @@ contract FundMe{
     }
 
     // function withdraw() public {}
-
-    function getPrice() public view returns(uint256) {
-        // Interface defines function signatures without implementation, enabling interaction with external contracts.
-        // Using the Chainlink AggregatorV3Interface to fetch the latest ETH/USD price from the deployed price feed contract.
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPriceUSD = getPrice();
-        uint256 ethCostUSD = (ethAmount * ethPriceUSD) / 1e18;
-        return ethCostUSD;
-    }
-
 }
---
+++ 3. Fund Me/2. PriceConverter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+// library is a special type of contract that contains reusable code, Cannot hold state varible and all functions are marked internal
+library PriceConverter{
+    function getPrice() internal view returns(uint256) {
+        // Interface defines function signatures without implementation, enabling interaction with external contracts.
+        // Using the Chainlink AggregatorV3Interface to fetch the latest ETH/USD price from the deployed price feed contract.
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPriceUSD = getPrice();
+        uint256 ethCostUSD = (ethAmount * ethPriceUSD) / 1e18;
+        return ethCostUSD;
+    }
+}
